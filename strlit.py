3d989b9048c29bedf796dabb5228640f7d089418
amirhosein farhangian <amirh.far8@gmail.com>
2025-06-28 15:16:00 +0330

    huge progress

--- strlit.py
+++ strlit.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from detect import run
 
+from vid_maker import vid_mkr
+
 st.set_page_config(
     page_title="Live CCTV Feed",
     page_icon="🧊",
@@ -26,23 +28,23 @@ THRESHOLD = 10
 frames = []
 with col1:
    # cam1 = st.text_input('cam #1', 'http://192.168.100.11:4747/video',key="cam1",label_visibility="collapsed",placeholder="cam #1 url")
-   cam1 = st.text_input('cam #1', 'http://10.0.0.7:4747/video',key="cam1",label_visibility="collapsed",placeholder="cam #1 url")
-   try:
-      frame = st.image(cam1,use_column_width =True)
-      frames.append(frame)
-      if len(frames==THRESHOLD):
-         video = np.ndarray(frames)
-         
-      
-      fire_det = run(weights="model/yolov5s_best.pt", source=frames, data="fire.yaml", conf_thres=0.2)
-      
-      loaded_frames = st.image(fire_det, use_column_width =True)
-      
+   cam1 = st.text_input('cam #1', 'http://192.168.100.16:4747/video',key="cam1",label_visibility="collapsed",placeholder="cam #1 url")
+   # try:
+   frame = st.image(cam1)
+   frames.append(frame)
+   fire_det = None
+   if len(frames) == THRESHOLD:
+      vid_mkr(frames=frames)
+      fire_det = run(weights="model/yolov5s_best.pt", source="saves/output.mp4", data="fire.yaml", conf_thres=0.2)
+      frames = []
+   
+   loaded_frames = st.image(fire_det)
+   
 
-      cam1_feed = loaded_frames
+   cam1_feed = loaded_frames
       # cam1_feed = st.image(cam1,use_column_width =True)
-   except:
-      st.image("Original-colour-bar.png")
+   # except:
+      # st.image("Original-colour-bar.png")
 # with col2:
 #    cam2 = st.text_input('cam #2', 'http://192.168.1.101:81/stream',key="cam2",label_visibility="collapsed",placeholder="cam #2 url")
 #    try:
