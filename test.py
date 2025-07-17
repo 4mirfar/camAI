2d9d56e3ab802bdbc64019905ed8a1134fe633ee
Amirhossein Farhangian <amirh.far8@gmail.com>
2025-07-17 08:46:58 -0700

    yolov5 not enough

--- test.py
+++ test.py
@@ -2,3 +2,4 @@
 from detect import run
 
 run(weights="models/yolov5s_best.pt", source="http://10.0.0.1:4747/video", conf_thres=0.2)
+run(weights="models/yolov5s_best.pt", source="http://192.168.100.19:4747/video", conf_thres=0.2)
